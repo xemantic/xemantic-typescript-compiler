6ff241db2ae93dcf28ed3c46a68c23b2d1f51ac3
Kazik Pogoda <morisil@xemantic.com>
2026-02-28 12:45:25 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,257 @@
+@file:OptIn(ExperimentalWasmDsl::class, ExperimentalKotlinGradlePluginApi::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jreleaser.model.Active
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.plugin.power.assert)
+    alias(libs.plugins.kotlinx.binary.compatibility.validator)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.versions)
+    alias(libs.plugins.maven.publish)
+    alias(libs.plugins.jreleaser)
+    alias(libs.plugins.xemantic.conventions)
+}
+
+// TODO change the group
+group = "com.xemantic.template"
+
+// TODO fill up the details
+xemantic {
+    description = "A template repository for Xemantic's Kotlin multiplatform projects"
+    inceptionYear = "2025"
+    applyAllConventions()
+}
+
+fun MavenPomDeveloperSpec.projectDevs() {
+    developer {
+        id = "morisil"
+        name = "Kazik Pogoda"
+        url = "https://github.com/morisil"
+    }
+}
+
+val javaTarget = libs.versions.javaTarget.get()
+val kotlinTarget = KotlinVersion.fromVersion(libs.versions.kotlinTarget.get())
+
+kotlin {
+
+    // TODO remove for a non-library project
+    explicitApi()
+
+    compilerOptions {
+        apiVersion = kotlinTarget
+        languageVersion = kotlinTarget
+        freeCompilerArgs.addAll(
+            "-Xcontext-parameters",
+            "-Xcontext-sensitive-resolution"
+        )
+        extraWarnings = true
+        progressiveMode = true
+        //optIn.addAll("add opt ins here")
+    }
+
+    jvm {
+        // set up according to https://jakewharton.com/gradle-toolchains-are-rarely-a-good-idea/
+        compilerOptions {
+            apiVersion = kotlinTarget
+            languageVersion = kotlinTarget
+            jvmTarget = JvmTarget.fromTarget(javaTarget)
+            freeCompilerArgs.add("-Xjdk-release=$javaTarget")
+            progressiveMode = true
+        }
+    }
+
+    js {
+        browser()
+        nodejs()
+        // TODO remove for a non-library project
+        binaries.library()
+    }
+
+    wasmJs {
+        browser()
+        nodejs()
+        d8()
+        // TODO remove for a non-library project
+        binaries.library()
+    }
+
+    wasmWasi {
+        nodejs()
+        // TODO remove for a non-library project
+        binaries.library()
+    }
+
+    // native, see https://kotlinlang.org/docs/native-target-support.html
+    // tier 1
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+    iosArm64()
+
+    // tier 2
+    linuxX64()
+    linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+
+    // tier 3
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX86()
+    androidNativeX64()
+    mingwX64()
+    watchosDeviceArm64()
+
+    swiftExport {}
+
+    sourceSets {
+
+        commonTest {
+            dependencies {
+                implementation(libs.kotlin.test)
+                implementation(libs.xemantic.kotlin.test)
+            }
+        }
+
+    }
+
+}
+
+repositories {
+    mavenCentral()
+}
+
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.ow2.asm") {
+            useVersion(libs.versions.asm.get())
+        }
+    }
+}
+
+// skip tests which require XCode components to be installed
+tasks {
+    named("tvosSimulatorArm64Test") { enabled = false }
+    named("watchosSimulatorArm64Test") { enabled = false }
+}
+
+// TODO only relevant for private projects, public project snapshots are released to maven central
+//publishing {
+//    if (isPublishingToGitHub) {
+//        repositories {
+//            maven {
+//                name = "GitHubPackages"
+//                url = uri("https://maven.pkg.github.com/xemantic/xemantic-kotlin-core")
+//                credentials(PasswordCredentials::class)
+//            }
+//        }
+//    }
+//}
+
+powerAssert {
+    functions = listOf(
+        "com.xemantic.kotlin.test.assert",
+        "com.xemantic.kotlin.test.have"
+    )
+}
+
+// https://kotlinlang.org/docs/dokka-migration.html#adjust-configuration-options
+dokka {
+    pluginsConfiguration.html {
+        footerMessage = xemantic.copyright
+    }
+}
+
+mavenPublishing {
+
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+
+    pom {
+
+        name = rootProject.name
+        description = xemantic.description
+        inceptionYear = xemantic.inceptionYear
+        url = "https://github.com/${xemantic.gitHubAccount}/${rootProject.name}"
+
+        organization {
+            name = xemantic.organization
+            url = xemantic.organizationUrl
+        }
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+
+        scm {
+            url = "https://github.com/${xemantic.gitHubAccount}/${rootProject.name}"
+            connection = "scm:git:git://github.com/${xemantic.gitHubAccount}/${rootProject.name}.git"
+            developerConnection = "scm:git:ssh://git@github.com/${xemantic.gitHubAccount}/${rootProject.name}.git"
+        }
+
+        ciManagement {
+            system = "GitHub"
+            url = "https://github.com/${xemantic.gitHubAccount}/${rootProject.name}/actions"
+        }
+
+        issueManagement {
+            system = "GitHub"
+            url = "https://github.com/${xemantic.gitHubAccount}/${rootProject.name}/issues"
+        }
+
+        developers {
+            projectDevs()
+        }
+
+    }
+
+}
+
+val releaseAnnouncementSubject = """🚀 ${rootProject.name} $version has been released!"""
+val releaseAnnouncement = """
+$releaseAnnouncementSubject
+
+${xemantic.description}
+
+${xemantic.releasePageUrl}
+""".trim()
+
+jreleaser {
+
+    announce {
+        webhooks {
+            create("discord") {
+                active = Active.ALWAYS
+                message = releaseAnnouncement
+                messageProperty = "content"
+                structuredMessage = true
+            }
+        }
+        linkedin {
+            active = Active.ALWAYS
+            subject = releaseAnnouncementSubject
+            message = releaseAnnouncement
+        }
+        bluesky {
+            active = Active.ALWAYS
+            status = releaseAnnouncement
+        }
+    }
+
+}
